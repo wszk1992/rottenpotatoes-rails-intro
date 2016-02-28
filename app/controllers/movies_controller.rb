550f031e70a1da8e1fa289904f813e67a1af4882
Kan Zheng <wszk1992@gmail.com>
2016-02-27 22:11:33 -0600

    return back

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,8 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    #@movies = Movie.all
-	@movies = Movie.order(title);
+    @movies = Movie.all
+	#@movies = Movie.all.order(:title);
   end
 
   def new
