4aa6deb482150bbc62a996e42be5d78510a1b468
Kan Zheng <wszk1992@gmail.com>
2016-03-04 21:32:36 -0600

    finished hw3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,17 @@
 class MoviesController < ApplicationController
-
+  helper_method :includeRatings?
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+
+  def includeRatings?(rating)
+    if params[:ratings]
+      params[:ratings].include?(rating)
+    elsif session[:cur_ratings]
+      session[:cur_ratings].include?(rating) 
+    else 
+      true 
+    end
   end
 
   def show
@@ -11,17 +21,49 @@ class MoviesController < ApplicationController
   end
 
   def index
-    case params[:sort]
-    when 'title'
-      @movies = Movie.order("title")
-      @title_hilite = 'hilite'
-    when 'release_date'
-      @movies = Movie.order("release_date")
-      @date_hilite = 'hilite'
+    @all_ratings = Movie.ratingValues
+    if params[:ratings]
+      session[:cur_ratings] = params[:ratings]
+      @movies = Movie.where(rating: session[:cur_ratings].keys)
+    elsif session[:cur_ratings]
+      @movies = Movie.where(rating: session[:cur_ratings].keys)
     else
       @movies = Movie.all
+      session[:cur_ratings] = Hash[@all_ratings.zip([1,1,1,1])]
+    end
+    if !params[:ratings]
+      flash.keep
+      if !session[:cur_ratings]
+        redirect_to movies_path
+      else
+        redirect_to movies_path(:ratings => session[:cur_ratings])
+      end
+    end
+    case params[:sort]
+    when 'title'
+      session[:cur_sort] = params[:sort]
+      @movies = @movies.order(session[:cur_sort])
+      @title_hilite = 'hilite'
+    when 'release_date'
+      session[:cur_sort] = params[:sort]
+      @movies = @movies.order(session[:cur_sort])
+      @date_hilite = 'hilite'
+    else
+        case session[:cur_sort]
+        when 'title'
+          @movies = @movies.order(session[:cur_sort])
+          @title_hilite = 'hilite'
+        when 'release_date'
+          @movies = @movies.order(session[:cur_sort])
+          @date_hilite = 'hilite'
+        else
+          @movies = @movies.all
+        end
     end 
+    
   end
+
+
 
   def new
     # default: render 'new' template
@@ -30,7 +72,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
+    redirect_to movies_path 
   end
 
   def edit
